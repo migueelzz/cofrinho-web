6691fb8a673bd6171ea33f003b5b930fbd8285c6
Miguel Lemes <miguellemes005@gmail.com>
2025-04-14 12:23:41 -0300

    chore: update timeout requests

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,5 +3,5 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3333',
   withCredentials: true,
-  timeout: 800,
+  timeout: 10000, // 10s
 })
